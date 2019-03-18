09b1054e6ea9cda3c4aa95815aac379e5621bbcb
premanand17 <prem.apa@gmail.com>
2019-03-18 15:05:03 +0000

    updated license for genome, operon, refseq_loader

--- tark/refseq_loader/tests/test_databasehandler.py
+++ tark/refseq_loader/tests/test_databasehandler.py
@@ -1,15 +1,25 @@
+"""
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 from django.test import TestCase
-from Bio.SeqFeature import SeqFeature, FeatureLocation
 import os
 from refseq_loader.handlers.refseq.fastahandler import FastaHandler
-from refseq_loader.handlers.refseq.gffhandler import GFFHandler
 from refseq_loader.handlers.refseq.databasehandler import SessionHandler,\
-    ReleaseHandler, GenomeHandler, AssemblyHandler, ReleaseSourceHandler,\
-    FeatureHandler, DatabaseHandler
-import gene
-from pip._vendor.requests.sessions import session
-import collections
-
+    GenomeHandler, AssemblyHandler, ReleaseSourceHandler, FeatureHandler
 
 '''
 /manage.py test refseq_loader.tests --settings=tark.settings.test
