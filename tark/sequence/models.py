49fac8379b9d175afa19185d159cc9281fda14f2
premanand17 <prem.apa@gmail.com>
2019-03-18 15:15:38 +0000

    updated license for sequence

--- tark/sequence/models.py
+++ tark/sequence/models.py
@@ -1,3 +1,21 @@
+"""
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
 from django.db import models
 from session.models import Session
 from tark.fields import ChecksumField
@@ -7,7 +25,6 @@ class Sequence(models.Model):
 
     seq_checksum = ChecksumField(max_length=20, blank=False, null=False, primary_key=True)
     sequence = models.TextField(blank=True, null=True)
-    # sequence = SequenceField(blank=True, null=True)
     session = models.ForeignKey(Session, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
