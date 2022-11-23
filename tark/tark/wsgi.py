eb6f27000ff25eb054b282ccd654a793d172b583
Sanjay Boddu <sboddu@ebi.ac.uk>
2022-11-23 13:58:17 +0000

    Pass OS env var to wsgi application

--- tark/tark/wsgi.py
+++ tark/tark/wsgi.py
@@ -18,15 +18,13 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-"""
-WSGI config for tark project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tark.settings.base")
 
-application = get_wsgi_application()
+env_variables_to_pass = ['DJANGO_ENVIRONMENT', ]
+
+def application(environ, start_response):
+    # pass the WSGI environment variables on through to os.environ
+    for var in env_variables_to_pass:
+        os.environ[var] = environ.get(var, '')
+
+    return get_wsgi_application()(environ, start_response)
